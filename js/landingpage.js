c3fdf4e586d852d222df2ec3817adfdcaec3214f
Vladimir Pronevich <epicfaiil138@gmail.com>
2019-02-19 15:13:41 +0200

    Create contact us. Code Refactoring. Writen html-side of the footer.

---
+++ js/landingpage.js
@@ -0,0 +1,74 @@
+const violetlight = '#784FBB';
+const violet = '#472898';
+const violetsuperLight = '#785EBC';
+const blue = '#250D63';
+const greylight = '#f2f2f2';
+const pinky = '#701F93';
+const fontcyrillic = 'MuseoSansCyrl,sans-serif';
+const fontbig = 'Montserrat, sans-serif';
+const fontitalic = 'Apercu Pro, sans-serif';
+const menu = $(".navbar-fixed-top");
+
+  $(window).scroll(function() {
+    var top = $(this).scrollTop();
+    if ( top >= 560 ) {
+      $(".tmp-clr").css('color',violet);
+      $(".first-text").css('color',violet);
+    } else if ( top <= 560 ) {
+        $(".tmp-clr").css('color','white');
+        $(".first-text").css('color','white');
+    }
+  });
+  
+$(function() {
+    $('.intro').addClass('go');
+  
+    $('.reload').click(function() {
+      $('.intro').removeClass('go').delay(200).queue(function(next) {
+        $('.intro').addClass('go');
+        next();
+      });
+  
+    });
+  })
+  $('textarea').blur(function () {
+    $('#hire textarea').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('textarea + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('textarea + label + span').css({'opacity': 0});
+        }
+    });
+});
+
+$('#hire .field:first-child input').blur(function () {
+    $('#hire .field:first-child input').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('.field:first-child input + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('.field:first-child input + label + span').css({'opacity': 0});
+        }
+    });
+});
+
+$('#hire .field:nth-child(2) input').blur(function () {
+    $('#hire .field:nth-child(2) input').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('.field:nth-child(2) input + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('.field:nth-child(2) input + label + span').css({'opacity': 0});
+        }
+    });
+});
--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,4 @@
-var menu = $(".navbar-fixed-top");
-  $(window).scroll(function() {
-    var top = $(this).scrollTop();
-    if ( top >= 100 ) {
-      console.log(1);
-      menu.$(".main-menu").$("span").css('color:red!important')
-    } else if ( top <= 200 ) {
-      menu.$(".main-menu").$("span").css('color:red!important')
-    }
-  });
+
 document.onmousemove = shiftImageXY;
 
 var tempX = 0;
@@ -41,55 +32,3 @@ function shiftImageXY(e) {
   oldTempY = tempY;
   return true;
 }
-$(function() {
-    $('.intro').addClass('go');
-  
-    $('.reload').click(function() {
-      $('.intro').removeClass('go').delay(200).queue(function(next) {
-        $('.intro').addClass('go');
-        next();
-      });
-  
-    });
-  })
-  $('textarea').blur(function () {
-    $('#hire textarea').each(function () {
-        $this = $(this);
-        if ( this.value != '' ) {
-          $this.addClass('focused');
-          $('textarea + label + span').css({'opacity': 1});
-        }
-        else {
-          $this.removeClass('focused');
-          $('textarea + label + span').css({'opacity': 0});
-        }
-    });
-});
-
-$('#hire .field:first-child input').blur(function () {
-    $('#hire .field:first-child input').each(function () {
-        $this = $(this);
-        if ( this.value != '' ) {
-          $this.addClass('focused');
-          $('.field:first-child input + label + span').css({'opacity': 1});
-        }
-        else {
-          $this.removeClass('focused');
-          $('.field:first-child input + label + span').css({'opacity': 0});
-        }
-    });
-});
-
-$('#hire .field:nth-child(2) input').blur(function () {
-    $('#hire .field:nth-child(2) input').each(function () {
-        $this = $(this);
-        if ( this.value != '' ) {
-          $this.addClass('focused');
-          $('.field:nth-child(2) input + label + span').css({'opacity': 1});
-        }
-        else {
-          $this.removeClass('focused');
-          $('.field:nth-child(2) input + label + span').css({'opacity': 0});
-        }
-    });
-});

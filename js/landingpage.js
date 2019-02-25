ebd1f9b215aeb74492a2d85cdb651f39b3309b44
Vladimir Pronevich <epicfaiil138@gmail.com>
2019-02-25 16:13:14 +0200

    Little changes

--- js/landingpage.js
+++ js/landingpage.js
@@ -8,79 +8,77 @@ const fontcyrillic = 'MuseoSansCyrl,sans-serif';
 const fontbig = 'Montserrat, sans-serif';
 const fontitalic = 'Apercu Pro, sans-serif';
 const menu = $(".navbar-fixed-top");
-$('.menu-toggle').on('click', function(){
+$('.menu-toggle').on('click', function () {
   $('body').toggleClass('open');
 });
-  $(window).scroll(function() {
-    var top = $(this).scrollTop();
-    if ( top >= 560 && top<5000) {
-      $(".first-text").css('color',violet);
-     // $("button: before").css('color',violet);
-      
-    } else if ( top <= 560 ) {
-      $(".first-text").css('color','white');
-      //  $("button :before").css('color','white');
-        
+$(window).scroll(function () {
+  var top = $(this).scrollTop();
+  if (top >= 560 && top < 5000) {
+    $(".first-text").css('color', violet);
+    // $("button: before").css('color',violet);
+
+  } else if (top <= 560) {
+    $(".first-text").css('color', 'white');
+    //  $("button :before").css('color','white');
+
+  }
+  if (top > 5000 && top < 5800) {
+    $(".first-text").css('color', 'white');
+  }
+  if (top > 5800) {
+    $(".first-text").css('color', violet);
+  }
+});
+
+$(function () {
+  $('.intro').addClass('go');
+
+  $('.reload').click(function () {
+    $('.intro').removeClass('go').delay(200).queue(function (next) {
+      $('.intro').addClass('go');
+      next();
+    });
+
+  });
+})
+$('textarea').blur(function () {
+  $('#hire textarea').each(function () {
+    $this = $(this);
+    if (this.value != '') {
+      $this.addClass('focused');
+      $('textarea + label + span').css({ 'opacity': 1 });
     }
-    if(top >5000 && top<5800)
-    {
-      $(".first-text").css('color','white');
-    }
-    if(top >5800)
-    {
-      $(".first-text").css('color',violet);
+    else {
+      $this.removeClass('focused');
+      $('textarea + label + span').css({ 'opacity': 0 });
     }
   });
-  
-$(function() {
-    $('.intro').addClass('go');
-  
-    $('.reload').click(function() {
-      $('.intro').removeClass('go').delay(200).queue(function(next) {
-        $('.intro').addClass('go');
-        next();
-      });
-  
-    });
-  })
-  $('textarea').blur(function () {
-    $('#hire textarea').each(function () {
-        $this = $(this);
-        if ( this.value != '' ) {
-          $this.addClass('focused');
-          $('textarea + label + span').css({'opacity': 1});
-        }
-        else {
-          $this.removeClass('focused');
-          $('textarea + label + span').css({'opacity': 0});
-        }
-    });
 });
 
 $('#hire .field:first-child input').blur(function () {
-    $('#hire .field:first-child input').each(function () {
-        $this = $(this);
-        if ( this.value != '' ) {
-          $this.addClass('focused');
-          $('.field:first-child input + label + span').css({'opacity': 1});
-        }
-        else {
-          $this.removeClass('focused');
-          $('.field:first-child input + label + span').css({'opacity': 0});
-        }
-    });
+  $('#hire .field:first-child input').each(function () {
+    $this = $(this);
+    if (this.value != '') {
+      $this.addClass('focused');
+      $('.field:first-child input + label + span').css({ 'opacity': 1 });
+    }
+    else {
+      $this.removeClass('focused');
+      $('.field:first-child input + label + span').css({ 'opacity': 0 });
+    }
+  });
 });
 
 $('#hire .field:nth-child(2) input').blur(function () {
-    $('#hire .field:nth-child(2) input').each(function () {
-        $this = $(this);
-        if ( this.value != '' ) {
-          $this.addClass('focused');
-          $('.field:nth-child(2) input + label + span').css({'opacity': 1});
-        }
-        else {
-          $this.removeClass('focused');
-          $('.field:nth-child(2) input + label + span').css({'opacity': 0});
-        }
-    });
+  $('#hire .field:nth-child(2) input').each(function () {
+    $this = $(this);
+    if (this.value != '') {
+      $this.addClass('focused');
+      $('.field:nth-child(2) input + label + span').css({ 'opacity': 1 });
+    }
+    else {
+      $this.removeClass('focused');
+      $('.field:nth-child(2) input + label + span').css({ 'opacity': 0 });
+    }
+  });
 });
--- js/scripts.js
+++ js/scripts.js
@@ -5,29 +5,29 @@ var tempX = 0;
 var tempY = 0;
 var oldTempX = 0;
 var oldTempY = 0;
-var IE =  !!(window.attachEvent&&!window.opera);
+var IE = !!(window.attachEvent && !window.opera);
 
 function shiftImageXY(e) {
-  if (IE) { 
+  if (IE) {
     tempX = event.clientX + document.body.scrollLeft;
     tempY = event.clientY + document.body.scrollTop;
-  } else {  
+  } else {
     tempX = e.pageX;
     tempY = e.pageY;
-  }  
+  }
   img = document.getElementById('preview-img');
   speedFactorDamping = 0.03; // change this for faster movement
-  xdir = (tempX - oldTempX) ;
-  ydir = (tempY - oldTempY) ;
-  parallexX = -xdir*speedFactorDamping;
-  parallexY = -ydir*speedFactorDamping;
+  xdir = (tempX - oldTempX);
+  ydir = (tempY - oldTempY);
+  parallexX = -xdir * speedFactorDamping;
+  parallexY = -ydir * speedFactorDamping;
   currX = parseInt(img.offsetLeft);
   currY = parseInt(img.offsetTop);
 
   img.style.left = (currX + parallexX) + 'px';
-  if(currY>0){
-  img.style.top = (currY + parallexY) + 'px';
-    }
+  if (currY > 0) {
+    img.style.top = (currY + parallexY) + 'px';
+  }
   oldTempX = tempX;
   oldTempY = tempY;
   return true;
